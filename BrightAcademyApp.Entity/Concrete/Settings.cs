d4a9f0b0e4290ee548a248665af17ca08e60b5a8
Erdinç Dursun <erdincdursun20@gmail.com>
2023-09-03 20:32:21 +0300

    Initial commit

---
+++ BrightAcademyApp.Entity/Concrete/Settings.cs
@@ -0,0 +1,21 @@
+﻿using BrightAcademyApp.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrightAcademyApp.Entity.Concrete
+{
+    public class Settings
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public string About { get; set; }
+        public string Contact { get; set; }
+        public string Questions { get; set; }
+
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# BrightAcademy
+ 
